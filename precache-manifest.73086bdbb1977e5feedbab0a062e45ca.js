75768aa7f157e87ece25b8f8ef132075387c9ac9
Caranatar <caranatar@riseup.net>
2020-07-13 00:51:06 -0400

    Updates

---
+++ precache-manifest.73086bdbb1977e5feedbab0a062e45ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64de37e6bf33ca0a245d7e22412fc4a",
+    "url": "/guestbook-client/index.html"
+  },
+  {
+    "revision": "0f5cedd631627fd29036",
+    "url": "/guestbook-client/static/css/main.b6b81a52.chunk.css"
+  },
+  {
+    "revision": "5e01f9be693048497e1d",
+    "url": "/guestbook-client/static/js/2.1e2c029a.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/guestbook-client/static/js/2.1e2c029a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5cedd631627fd29036",
+    "url": "/guestbook-client/static/js/main.6879338b.chunk.js"
+  },
+  {
+    "revision": "5a562aa302fb3e9b0803",
+    "url": "/guestbook-client/static/js/runtime-main.db7dc33c.js"
+  }
+]);
