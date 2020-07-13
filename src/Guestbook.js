df3976bb5ead613aff12dddb8c4844355c836d05
Caranatar <caranatar@riseup.net>
2020-07-13 01:11:03 -0400

    Fixing redirect URI

--- src/Guestbook.js
+++ src/Guestbook.js
@@ -49,7 +49,7 @@ const Guestbook = () => {
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
+      redirectUri={window.location.href}
       audience={process.env.REACT_APP_AUTH0_AUDIENCE}
       scope="update:posts"
     >
